75f044dce4ee28b20edffa9074e36ae2605b103c
likeroge <likeroge@gmail.com>
2022-06-08 16:21:44 +0400

    updated compare function

--- src/utils/sortFunctions.js
+++ src/utils/sortFunctions.js
@@ -1,8 +1,8 @@
 export const notesSortByNumber = (a, b) => {
-  if (a.number < b.number) {
+  if (+a.number < +b.number) {
     return -1;
   }
-  if (a.number > b.number) {
+  if (+a.number > +b.number) {
     return 1;
   }
 
